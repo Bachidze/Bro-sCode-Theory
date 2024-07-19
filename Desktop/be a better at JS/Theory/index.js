2e938f1cf3fa0e1c429052e5566a28444e79ea1b
bachidze <bachidze90@gmail.com>
2024-07-19 15:02:50 +0400

    Feat(Add):theory

---
+++ Desktop/be a better at JS/Theory/index.js
@@ -0,0 +1,78 @@
+// #1 დომში გამოიტანს იმ ტექსტს,რასაც გადავცემთ ინფუთში
+
+// let username
+// document.getElementById("myButton").onclick = function(){
+//     username = document.getElementById("myText").value
+//     console.log(username)
+//     document.getElementById("myLabel").innerHTML ="hello" + username
+// }
+
+
+
+// #2 ფრომფთში რა ასაკსაც გადავცემთ უმატებს ერთს და გილოცავს.
+// #2 x,y,z JavaScript-ის ტიპები(არასრული)
+
+/* let age = window.prompt("How Old Are You")
+age = Number(age)
+age += 1
+
+
+console.log("Happy Birthday! You are",age,"Years Old") */
+
+
+/* let x
+let y
+let z
+
+x = Number("3.14")
+y = String(3.14)
+z = Boolean("")
+console.log(x, typeof x)
+console.log(y, typeof y)
+console.log(z, typeof z) */
+
+
+
+// #3 const-რომელიც არ იცვლება
+// #3 რადიუსის ფორმულა C=2pr
+
+/* const p = 3.14159
+let radius
+let circumference
+
+
+radius = window.prompt("Enter the radius of a circle")
+radius = Number(radius)
+circumference = 2 * p * radius
+
+console.log("The circumference is:", circumference) */
+
+
+// #4 Math
+
+
+// let x = 3.14
+// let y = 5
+// let z = 9
+// let maximum
+// let minimum
+
+
+// x = Math.round(x) // მათემატიკურად ამრგვალებს 3.1 = 3 || 3.5 = 4
+// x = Math.floor(x) // ყოველთვის ამრგვალებს ნაკლებობისკენ 3.9 = 3
+// x = Math.ceil(x) // ყოველთვის ამრგვალებს მეტობისკენ 3.1 = 4
+// x = Math.pow(x,3) // მეორე პარამეტრად გადაეცემა რიცხვი,რომელზეც მრავლდება
+// x = Math.sqrt(x) // იღებს ფესვს
+//x = Math.abs(x) // ნეგატივი რიცხვი გადაჰყავს დადებითში
+
+// x = Math.PI
+
+// maximum = Math.max(x,y,z)
+// minimum = Math.min(x,y,z)
+
+// console.log(maximum)
+// console.log(minimum)
+// console.log(x)
+
+
+
